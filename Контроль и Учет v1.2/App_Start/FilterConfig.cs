a9eb02297be902092a36d100e65133449940bb37
RakhmanovSO <42904303+RakhmanovSO@users.noreply.github.com>
2019-12-01 19:18:59 +0300

    Add files via upload

---
+++ Контроль и Учет v1.2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Контроль_и_Учет_v1._2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
